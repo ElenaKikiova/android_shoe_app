c5bde6a262d34929488e403b00fc540380f9f134
elena.kikiova <elena.kikiova@primeholding.com>
2023-11-14 15:57:48 +0200

    Shoes in List view

---
+++ app/src/main/java/com/example/shoes/ShoeListItem.java
@@ -0,0 +1,14 @@
+package com.example.shoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoeListItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoe_list_item);
+    }
+}
