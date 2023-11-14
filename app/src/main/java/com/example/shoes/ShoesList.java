2ae9e30d77841b278761b9746b245f65ca255397
elena.kikiova <elena.kikiova@primeholding.com>
2023-11-14 12:01:42 +0200

    Shoe Details

--- app/src/main/java/com/example/shoes/ShoesList.java
+++ app/src/main/java/com/example/shoes/ShoesList.java
@@ -2,18 +2,27 @@ package com.example.shoes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 
 public class ShoesList extends AppCompatActivity {
 
+    protected FloatingActionButton addShoeButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shoes_list);
+        addShoeButton = findViewById(R.id.add_shoe_button);
         
         DatabaseHelper dbHelper = new DatabaseHelper(getApplicationContext());
 
@@ -23,5 +32,16 @@ public class ShoesList extends AppCompatActivity {
 
         ListView listView = (ListView) findViewById(R.id.shoes_list);
         listView.setAdapter(adapter);
+
+
+        addShoeButton.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent i = new Intent(ShoesList.this, ShoeDetails.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
 }
