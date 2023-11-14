08678620e48bb52a96ce5b4984f5890d333b5d0a
elena.kikiova <elena.kikiova@primeholding.com>
2023-11-14 12:01:30 +0200

    Shoe Details

---
+++ app/src/main/java/com/example/shoes/ShoeDetails.java
@@ -0,0 +1,14 @@
+package com.example.shoes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShoeDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shoe_details);
+    }
+}
